5c859e053fa5d0c461dba620977040ee3cf8ced1
ShravaniMaale <75345255+ShravaniMaale@users.noreply.github.com>
2021-04-27 11:43:57 -0400

    main

--- frontend/src/screens/ContactUsScreen.js
+++ frontend/src/screens/ContactUsScreen.js
@@ -1,105 +1,107 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import VMC from '../components/VMC'
+import MetaTag from '../components/MetaTag'
 
 const ContactUsScreen = () => {
   return (
-    <div>
-      <section className='section'>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-md-12 text-center'>
-              <h3 className='main-heading'>Our Company</h3>
-              <div className='underline mx-auto'></div>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged. It was
-                popularised in the 1960s with the release of Letraset sheets
-                containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of
-                Lorem Ipsum.
-              </p>
-              <Link to='/contactus' className='btn btn-warning shadow'>
-                Read More
-              </Link>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      {/* Our Vision, mission and values */}
-      <VMC />
-
-      {/* Testimonials */}
-      <section className='VMCsection bg-c-light border-top'>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-md-12 text-center'>
-              <h3 className='main-heading'>Testimonials</h3>
-              <div className='underline mx-auto'></div>
-            </div>
-            <div className='col-md-4'>
-              <div className='card shadow'>
-                <img
-                  src='/images/shravani.jpg'
-                  className='img border-bottom'
-                  alt='author1'
-                />
-                <hr />
-                <div className='card-body'>
-                  <h6>Description</h6>
-                  <div className='underline'></div>
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className='col-md-4'>
-              <div className='card shadow'>
-                <img
-                  src='/images/koushik.jpeg'
-                  className='img border-bottom'
-                  alt='author2'
-                />
-                <hr />
-                <div className='card-body'>
-                  <h6>Description</h6>
-                  <div className='underline'></div>
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className='col-md-4'>
-              <div className='card shadow'>
-                <img
-                  src='/images/saiganesh.jpg'
-                  className='img border-bottom'
-                  alt='author3'
-                />
-                <hr />
-                <div className='card-body'>
-                  <h6>Description</h6>
-                  <div className='underline'></div>
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry.
-                  </p>
-                </div>
+    <>
+      <MetaTag title={'Contact Us'} />
+      <div>
+        <section className='section'>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-md-12 text-center'>
+                <h3 className='main-heading'>Our Company</h3>
+                <div className='underline mx-auto'></div>
+                <p>
+                Sello Marketplace is for selling and buying goods. 
+                The website's foremost goal is to get all the retail/people-sellers to register and upload sellable items with proper images, 
+                descriptions, and details about the listed item. Encourages more sellers to advertise their products/ List 
+                with the best price on the website. 
+                From the user perspective, anyone/buyers can buy the products/items listed on the page. 
+                Everyone needs to register on the website to access/buy any item on the website. 
+                Users/customers can add the items into the cart and checkout, where sellers can also update their product and 
+                offers list at any time after the listing has been published. We are providing a user-friendly payment gateway for the purchases.
+                </p>
+                <Link to='/contactus' className='btn btn-warning shadow'>
+                  Read More
+                </Link>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-    </div>
+        </section>
+
+        {/* Our Vision, mission and values */}
+        <VMC />
+
+        {/* Testimonials */}
+        <section className='VMCsection bg-c-light border-top'>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-md-12 text-center'>
+                <h3 className='main-heading'>Testimonials</h3>
+                <div className='underline mx-auto'></div>
+              </div>
+              <div className='col-md-4'>
+                <div className='card shadow'>
+                  <img
+                    src='/images/shravani.jpg'
+                    className='img border-bottom'
+                    alt='author1'
+                  />
+                  <hr />
+                  <div className='card-body'>
+                    <h6>Details</h6>
+                    <div className='underline'></div>
+                    <p>
+                      LinkedIn: <a href='https://www.linkedin.com/in/shravani-male-622029195/'>ShravaniMale-LinkedIn</a><br></br>
+                      G-mail: <a href = "mailto: shravanireddymaale@gmail.com">shravanireddymaale@gmail.com</a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className='col-md-4'>
+                <div className='card shadow'>
+                  <img
+                    src='/images/koushik.jpeg'
+                    className='img border-bottom'
+                    alt='author2'
+                  />
+                  <hr />
+                  <div className='card-body'>
+                    <h6>Details</h6>
+                    <div className='underline'></div>
+                    <p>
+                      LinkedIn: <a href='https://www.linkedin.com/in/gururkguduru/'>GuruRamKoushik-LinkedIn</a><br></br>
+                      G-mail: <a href = "mailto: gururamkoushik@gmail.com">gururamkoushik@gmail.com</a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className='col-md-4'>
+                <div className='card shadow'>
+                  <img
+                    src='/images/saiganesh.jpg'
+                    className='img border-bottom'
+                    alt='author3'
+                  />
+                  <hr />
+                  <div className='card-body'>
+                    <h6>Details</h6>
+                    <div className='underline'></div>
+                    <p>
+                      LinkedIn: <a href='https://www.linkedin.com/in/sai-ganesh-mokhamatla/'>SaiGanesh-LinkedIn</a><br></br>
+                      G-mail: <a href = "mailto: ganeshsai287@gmail.com">ganeshsai287@gmail.com</a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
   )
 }
 
